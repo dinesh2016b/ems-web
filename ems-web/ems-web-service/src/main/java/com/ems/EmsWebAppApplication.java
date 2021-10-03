d1c6253941edff9c6a68b0d88aba8660546ef4de
Dinesh B <dinesh2016b@gmail.com>
2021-10-03 13:38:36 -0400

    Updated code to run application in external Tomcat and Jboss server and JWT authentication updates

--- ems-web/ems-web-service/src/main/java/com/ems/EmsWebAppApplication.java
+++ ems-web/ems-web-service/src/main/java/com/ems/EmsWebAppApplication.java
@@ -1,12 +1,13 @@
 package com.ems;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmsWebAppApplication extends SpringBootServletInitializer {
+public class EmsWebAppApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -15,5 +16,11 @@ public class EmsWebAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsWebAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 }
