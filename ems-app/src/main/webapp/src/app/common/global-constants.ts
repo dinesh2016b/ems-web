fb76b41286b1cff4f5be1baf7a4acd5a17593205
Dinesh B <dinesh2016b@gmail.com>
2023-02-14 14:16:16 -0500

    Updated angular.json configuration file to update optimaization and hashing

--- ems-app/src/main/webapp/src/app/common/global-constants.ts
+++ ems-app/src/main/webapp/src/app/common/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-    static readonly ENDPOINT_EMPLOYEE_URL: string = "http://localhost:8080/employees";
-    static readonly ENDPOINT_DEPARTMENT_URL: string = "http://localhost:8080/departments";
-    static readonly ENDPOINT_SALARIES_URL: string = "http://localhost:8080/ems-salaries";
+    static readonly ENDPOINT_EMPLOYEE_URL: string = "http://localhost:80/employees";
+    static readonly ENDPOINT_DEPARTMENT_URL: string = "http://localhost:80/departments";
+    static readonly ENDPOINT_SALARIES_URL: string = "http://localhost:80/ems-salaries";
 }
